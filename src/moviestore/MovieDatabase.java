6e7b51b139f724c0244da8e33e08218fa12b385f
jitaf <153903245+jitaf@users.noreply.github.com>
2025-02-26 16:57:34 +0100

    Update MovieDatabase.java

--- src/moviestore/MovieDatabase.java
+++ src/moviestore/MovieDatabase.java
@@ -11,7 +11,7 @@ public class MovieDatabase {
      */
     public void addMovies(int count) {
         //movies = new ....
-        for (int i=0; i<count; count++){
+        for (int i=0; i<count; i++){
             //nacti od uzivatele nazev, rok natoceni, delku filmu a zanr
             //pridej film do pole filmu
         }
